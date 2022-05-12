9acfccb84c6ddd36b017a256a5824a6fde90f5cc
Peam <peammy2010@hotmail.com>
2022-05-12 13:41:40 +0700

    check protoc before run

--- generate_proto.sh
+++ generate_proto.sh
@@ -1,8 +1,18 @@
 #! /bin/bash
+if [ ! $(which protoc) ]; then
+    echo "protoc not found: Please install protoc"
+    exit 1
+fi
+# each file in current dir
 for Dir in *; do
+    # if dir
     if [ -d "$Dir" ]; then
+
         cd $Dir
+        # get proto file
         file=$(ls | grep ".proto$")
+
+        # if file exist
         if [ $file ]; then
             echo "proto file in $Dir is $file"
             # generate proto file
